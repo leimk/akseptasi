96c0b8fe8efffcc579c68559741b51ddb6ebf5e2
Michael K Lei <michaelkusliawan@gmail.com>
2020-06-08 21:41:23 +0700

    Tambah form akseptasi di index

--- public/index.php
+++ public/index.php
@@ -7,7 +7,90 @@
     <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
 </head>
 <body>
-    <H1 class="bg-dark text-white text-center"><strong>AKSEPTASI</strong></H1>
+    <section id="header">
+        <H1 class="bg-dark text-white text-center"><strong>AKSEPTASI</strong></H1>
+    </section>
+    <section id="form-akseptasi">
+        <div class="container border border-dark">
+            <div class="col-lg-12">
+                <div class="col-lg-12 text-left">
+                    <h2>PT Asuransi Eka Lloyd Jaya</h2>
+                </div>
+            </div>
+            <div class="row"></div>
+            <div class="col-lg-12">
+                <div class="col-lg-4">
+                    <strong>
+                        <h3>Cabang:<b><u>Pekanbaru</u></b></h3>
+                    </strong>
+                </div>
+                <div class="col-lg-4">
+                    <div class="input-group mb-3">
+                        <div class="input-group-prepend">
+                            <div class="input-group-text bg-dark text-white">
+                                <input type="checkbox" aria-label="Jenis Polis" >&nbsp;Direct Policy<br>
+                            </div>
+                            <div class="input-group-text  bg-dark text-white">
+                                <input type="checkbox" aria-label="Jenis Polis">&nbsp;Coinsurance - Member<br>
+                            </div>
+                            <div class="input-group-text bg-dark text-white">
+                                <input type="checkbox" aria-label="Jenis Polis">&nbsp;New Policy<br>
+                            </div>
+                            <div class="input-group-text bg-dark text-white">
+                                <input type="checkbox" aria-label="Jenis Polis">&nbsp;Renewal Policy<br>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="container border border-dark">
+            <div class="col-lg-12 TEXT-CENTER bg-dark text-white ">
+                <h1 class="text-center"><strong><b>RISK ASSESSMENT FORM  [FIRE INSURANCE]</B></strong></h1>
+            </div>
+            <div class="row"></div>
+            <div class="col-lg-12 row">                
+                <div class="input-group input-group-sm mb-3 text-left">
+                    <div class="input-group-prepend text-left">
+                        <span class="input-group-text bg-dark text-white" id="inputGroup-sizing-sm"><b>1.Policy No (Renewal)</b></span>
+                    </div>
+                    <input type="text" class="form-control  border border-dark" aria-label="Small" aria-describedby="inputGroup-sizing-sm" placeholder="Nomor Polis lama">
+                </div>
+                <div class="input-group input-group-sm mb-3 text-left">
+                    <div class="input-group-prepend text-left">
+                        <span class="input-group-text bg-dark text-white" id="inputGroup-sizing-sm"><b>2.Insured Name</b></span>
+                    </div>
+                    <input type="text" class="form-control border border-dark" aria-label="Small" aria-describedby="inputGroup-sizing-sm" placeholder="Nama tertanggung">
+                </div>
+                <div class="input-group input-group-sm mb-3 text-left">
+                    <div class="input-group-prepend text-left">
+                        <span class="input-group-text bg-dark text-white" id="inputGroup-sizing-sm"><b>3.Insured Address</b></span>
+                    </div>
+                    <textarea type="text" class="form-control border border-dark" aria-label="Small" aria-describedby="inputGroup-sizing-sm" placeholder="Alamat tertanggung"></textarea>
+                </div>
+                <div class="input-group input-group-sm mb-3 text-left">
+                    <div class="input-group-prepend text-left">
+                        <span class="input-group-text bg-dark text-white" id="inputGroup-sizing-sm"><b>4.Location of Risk</b></span>
+                    </div>
+                    <textarea type="text" class="form-control border border-dark" aria-label="Small" aria-describedby="inputGroup-sizing-sm" placeholder="Alamat pertanggungan"></textarea>
+                </div>
+                <div class="input-group input-group-sm mb-3 text-left">
+                    <div class="input-group-prepend text-left">
+                        <span class="input-group-text bg-dark text-white" id="inputGroup-sizing-sm"><b>5.Occupation</b></span>
+                    </div>
+                    <input type="text" class="form-control border border-dark" aria-label="Small" aria-describedby="inputGroup-sizing-sm" placeholder="Okupasi">
+                </div>
+                <div class="input-group input-group-sm mb-3 text-left">
+                    <div class="input-group-prepend text-left">
+                        <span class="input-group-text bg-dark text-white" id="inputGroup-sizing-sm"><b>6.Periode of insurance</b></span>
+                    </div>
+                    <input type="text" class="form-control border border-dark" aria-label="Small" aria-describedby="inputGroup-sizing-sm" placeholder="Alamat tertanggung">
+                </div>
+            </div>
+
+
+        </div>
+    </section>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.bundle.min.js" integrity="sha384-1CmrxMRARb6aLqgBO7yyAxTOQE2AKb9GfXnEo760AUcUmFx3ibVJJAzGytlQcNXd" crossorigin="anonymous"></script>
 </body>
 </html>
